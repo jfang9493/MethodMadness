157d20fe53d3fca5c6c9ab5c0fd6cc2ad7b38a66
BT_1N3_26@bktech.local <BT_1N3_26@bktech.local>
2018-09-18 09:16:36 -0400

    Initial commit

---
+++ src/JFang/JFangLib.java
@@ -0,0 +1,8 @@
+package JFang;
+
+public class JFangLib {
+    public static void println(String str)
+    {
+        System.out.println(str);
+    }
+}
---
+++ src/JFang/TestFile.java
@@ -0,0 +1,10 @@
+package JFang;
+
+import static JFang.JFangLib.println;
+
+public class TestFile {
+    public static void main(String[] args)
+    {
+        println(":^)");
+    }
+}
